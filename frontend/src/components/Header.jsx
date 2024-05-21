ba696328a1a96798bc771a55dff18ad2e14562f6
Danila <kuhnovets.work@mail.ru>
2024-05-21 12:01:29 +0300

    add header and maint

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+    return(
+        <>
+        <div className="header">
+            <div className="heaedr_logo">
+               <div> <h1>RRResault</h1></div>
+                <div><h1>timer:3123123</h1></div>
+            </div>
+        </div>
+        
+        <div className="heaeder_text">
+                <p>Доброго дня Yandex</p>
+            </div>
+       
+        </>
+    
+    )
+
+};
---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,21 @@
+export default function Main() {
+    return(
+        <>
+        <div className="main">
+            <div>
+                <button className="btn_start">Start</button>
+            </div>
+            <div className="btn_2">
+                <button>Создать тест</button>
+                <button>Редактировать тест</button>
+            </div>
+            <div className="btn_3">
+                <button>Статистика</button>
+            </div>
+            </div>
+       
+        </>
+    
+    )
+
+};
