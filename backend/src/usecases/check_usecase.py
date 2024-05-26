1ffa109759e36e007a98d3b4c3ae07e02efe069e
Vadim Yuhnevich <yuhnevich.work@gmail.com>
2024-05-27 01:40:08 +0300

    fix

--- backend/src/usecases/check_usecase.py
+++ backend/src/usecases/check_usecase.py
@@ -1,3 +1,4 @@
+from core.exceptions import RequestProcessingException
 from ports.repository.cache_repository import CacheRepository
 
 
@@ -6,8 +7,11 @@ class CheckUsecase:
         self.cache_repo = cache_repo
 
     async def get_info(self, room_id) -> dict:
-        room_data = await self.cache_repo.get_cache(room_id)
-        question = room_data['test_data'][room_data["current_question_id"]]
-        if not room_data['isVisible']:
-            del question['answer']
-        return question
+        try:
+            room_data = await self.cache_repo.get_cache(room_id)
+            question = room_data['test_data'][room_data["current_question_id"]]
+            if not room_data['isVisible']:
+                del question['answer']
+            return question
+        except KeyError:
+            raise RequestProcessingException("Тест завершен")
