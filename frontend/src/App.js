06a6162f3546660fe4ac067c922a311fa826870c
Danila <kuhnovets.work@mail.ru>
2024-05-27 00:23:29 +0300

    fixm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,62 @@
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Main from './layouts /components/Main';
+import Main from '../src/layouts /components/Main';
 import Room from './layouts /room/Room';
-import Register from './layouts /auth/register/Register';
-import LogIn from './layouts /auth/login/LogIn';
-import CreateTest from './layouts /test/CreateTest';
-import UpdateTest from './layouts /test/UpdateTest';
-import StatisticTest from './layouts /statistics/StatisticTest';
-import Check from './layouts /check/Check';
+import Register from '../src/layouts /auth/register/Register';
+import LogIn from '../src/layouts /auth/login/LogIn';
+import CreateTest from '../src/layouts /test/CreateTest';
+import UpdateTest from '../src/layouts /test/UpdateTest';
+import StatisticTest from '../src/layouts /statistics/StatisticTest';
+import Check from '../src/layouts /check/Check';
+import ProtectedRoute from './layouts /auth/ProtectedRoute';
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<LogIn />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/room/:room_id" element={<Room />} />
         <Route path="/check/:room_id" element={<Check />} />
-        <Route path="/createTest" element={<CreateTest />} />
-        <Route path="/updateTest" element={<UpdateTest />} />
-        <Route path="/statisticTest" element={<StatisticTest />} />
+        <Route
+          path="/main"
+          element={
+            <ProtectedRoute>
+              <Main />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/room/:room_id"
+          element={
+            <ProtectedRoute>
+              <Room />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/createTest"
+          element={
+            <ProtectedRoute>
+              <CreateTest />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/updateTest"
+          element={
+            <ProtectedRoute>
+              <UpdateTest />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/statisticTest"
+          element={
+            <ProtectedRoute>
+              <StatisticTest />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </div>
